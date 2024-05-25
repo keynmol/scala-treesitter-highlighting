3771ccab835e05893f552a3f267562377eb65152
Anton Sviridov <keynmol@gmail.com>
2024-05-25 16:47:55 +0100

    It's alive!

--- index.scala
+++ index.scala
@@ -81,10 +81,57 @@ enum Switch:
   case Off(cls: String)
 
 def app(lang: Parser.Language) =
-  val code = Var(
+  val codeVar = Var(
     """
+@js.native
+@JSImport("/tree-sitter.js", JSImport.Default)
 object Parser extends js.Any:
-  def hello = 1
+  def parse(path: String): Tree = js.native
+
+  def getLanguage(): Language = js.native
+
+  @js.native
+  trait Language extends js.Any:
+    def query(source: String): Query = js.native
+
+  @js.native
+  trait Query extends js.Any:
+    def matches(node: Node): A[Match] = js.native
+    def captures(node: Node): A[Capture] = js.native
+
+  @js.native
+  trait Tree extends js.Any:
+    val rootNode: Node = js.native
+
+  @js.native
+  trait Node extends js.Any:
+    val id: Int = js.native
+    val children: A[Node] = js.native
+    val text: String = js.native
+    val startPosition: Point = js.native
+    val endPosition: Point = js.native
+
+  @js.native
+  trait Match extends js.Any:
+    val name: String = js.native
+
+  @js.native
+  trait Capture extends js.Any:
+    val name: String = js.native
+    val node: Node = js.native
+    val text: js.UndefOr[String] = js.native
+
+  @js.native
+  trait TreeCursor extends js.Any:
+    val nodeId: Int = js.native
+    val nodeText: String = js.native
+
+  @js.native
+  trait Point extends js.Any:
+    val row: Int = js.native
+    val column: Int = js.native
+end Parser
+
 """.trim
   )
   val query = lang.query(highlightQueries)
@@ -92,10 +139,10 @@ object Parser extends js.Any:
     textArea(
       rows := 5,
       cols := 50,
-      onInput.mapToValue --> code,
-      value <-- code
+      onInput.mapToValue --> codeVar,
+      value <-- codeVar
     ),
-    pre(children <-- code.signal.map { value =>
+    code(pre(children <-- codeVar.signal.map { value =>
       val tree = Parser.parse(value)
       val index = Index(value)
       val matches = query.captures(tree.rootNode)
@@ -106,7 +153,7 @@ object Parser extends js.Any:
             (
               index.resolve(capture.node.startPosition),
               index.resolve(capture.node.endPosition),
-              capture.name
+              capture.name.replace('.', '-')
             )
           )
 
@@ -114,24 +161,30 @@ object Parser extends js.Any:
 
       // var current: String | Null = null
       var idx = 0
-      // var textLength = 0 
+      // var textLength = 0
 
       println(index.mapping)
 
-
-      switches.distinctBy(x => (x._1, x._2)).foreach:
-        case (start, end, what) =>
-          // if what != current then 
+      switches
+        .distinctBy(x => (x._1, x._2))
+        .foreach:
+          case (start, end, what) =>
+            // if what != current then
             val textLength = start - idx
-            if textLength != 0 then 
+            if textLength != 0 then
               elements.addOne(span(value.slice(idx, start)))
-            elements.addOne(span(cls := what, styleAttr := "font-weight:bold", value.slice(start, end)))
+            elements.addOne(
+              span(
+                cls := what,
+                value.slice(start, end)
+              )
+            )
             idx = end
-          // elements.addOne(p(start, " ", end, " ", what))
+      // elements.addOne(p(start, " ", end, " ", what))
 
       elements.result()
 
-    })
+    }))
   )
 end app
 
